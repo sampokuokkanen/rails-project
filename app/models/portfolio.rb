a67dcfa8bf5f751d6dd14750492bcd8c8eaa624e
Sampo Kuokkanen <sampokuokkanen@SamponoMacBook-puro.local>
2019-05-27 23:10:08 +0900

    Rails defaults in model learned

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,5 +3,16 @@
 class Portfolio < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
-  validates_presence_of :title, :main_image, :thumb_image
+  validates_presence_of :title, :body, :main_image, :thumb_image
+
+  def self.angular
+    where(subtitle: 'Angular!')
+  end
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= 'https://via.placeholder.com/350x150'
+    self.thumb_image ||= 'https://via.placeholder.com/50x50'
+  end
 end
