f2a826063e43d956ccd01570bacddb5d301a09ac
Sampo Kuokkanen <sampokuokkanen@SamponoMacBook-puro.local>
2019-05-28 22:43:19 +0900

    Learned about concerns in Rails

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Portfolio < ApplicationRecord
+  include Placeholder
   extend FriendlyId
   friendly_id :title, use: :slugged
   validates_presence_of :title, :body, :main_image, :thumb_image
@@ -12,7 +13,7 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= 'https://via.placeholder.com/350x150'
-    self.thumb_image ||= 'https://via.placeholder.com/50x50'
+    self.main_image ||= Placeholder.image_generator(height: 600, width: 400)
+    self.thumb_image ||= Placeholder.image_generator(height: 350, width: 200)
   end
 end
