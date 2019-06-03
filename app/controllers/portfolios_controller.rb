01ba5d8c416f2fca1fda2ac5f9532243d0123e45
Sampo Kuokkanen <sampo.kuokkanen@gmail.com>
2019-06-03 23:03:06 +0900

    Refactored strong params

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -14,8 +14,7 @@ class PortfoliosController < ApplicationController
 
   # POST /portfolios
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
-
+    @portfolio_item = Portfolio.new(portfolio_params)
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Your portfolio is now live.' }
@@ -35,7 +34,7 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
         format.json { render :show, status: :ok, location: @blog }
       else
@@ -57,5 +56,12 @@ class PortfoliosController < ApplicationController
       format.html { redirect_to portfolios_url, notice: 'The record was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+  private
+  def portfolio_params
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      technologies_attributes: [:name])
   end
 end
