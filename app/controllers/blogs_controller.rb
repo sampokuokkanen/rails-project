f97aaf618e9a8e209865d765a8a763dc0627e5f7
Sampo Kuokkanen <sampo.kuokkanen@gmail.com>
2019-06-16 00:23:15 +0900

    Implemented authorization for portfolios and blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,8 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :toggle_status, :destroy]
   layout "blog"
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
+
   # GET /blogs
   # GET /blogs.json
   def index
