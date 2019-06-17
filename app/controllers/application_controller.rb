1d153757df2099a4e06c520f6abd511beaefd2bf
Sampo Kuokkanen <sampo.kuokkanen@gmail.com>
2019-06-17 12:39:20 +0900

    Updated master layout styles and helper methods and bootstrapified things

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,4 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
-
-  before_action :set_copyright
-
-  def set_copyright
-    @copyright = SampoViewTool::Renderer.copyright 'Sampo Kuokkanen', 'All Rights Reserved'
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
-  def login_helper
+  def login_helper style
     if current_user.is_a?(GuestUser)
-      (link_to "Login", new_user_session_path) + "<br>".html_safe +
-      (link_to "Register", new_user_registration_path)
+      (link_to "Login", new_user_session_path, class: style) + " ".html_safe +
+      (link_to "Register", new_user_registration_path, class: style)
     else
-      link_to "Logout", destroy_user_session_path, method: :delete
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
     end
   end
 
@@ -13,5 +13,9 @@ module ApplicationHelper
       greeting = "Thanks for visiting from #{session[:source]}"
       content_tag(:p, greeting, class: "source-greeting")
     end
+  end
+
+  def copyright_generator
+      SampoViewTool::Renderer.copyright 'Sampo Kuokkanen', 'All Rights Reserved'
   end
 end
